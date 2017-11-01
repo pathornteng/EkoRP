3aa755cfbf5c6468ee3f241c5e3934176a47299a
“pathorn” <pathorn@etda.or.th>
2017-11-01 22:18:44 +0700

    alpha release

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # EkoRP
+
+EkoRP is a test repo for Eko OpenID. 
+
+### Install
+- `npm install`
+
+### Config
+- `cp .env.example .env`
+- `vi .env`
+
+### Run
+- `npm start`
--- routes/index.js
+++ routes/index.js
@@ -2,24 +2,27 @@ var express = require('express');
 var router = express.Router();
 var rp = require('request-promise');
 var co = require('co');
+var jwtDecode = require('jwt-decode');
 
 /* OpenID server variable */
-var clientId = 'test';
-var clientSecret = 'testSecret';
-var redirectUri = 'http://localhost:3000/cb';
-var grant_type = 'authorization_code';
+var clientId = process.env.CLIENT_ID;
+var clientSecret = process.env.CLIENT_SECRET;
+var redirectUri = process.env.REDIRECT_URI;
+var tokenUri = process.env.TOKEN_URI;
+var userinfoUri = process.env.USERINFO_URI;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-function getToken(code) {
+/* This function is used to get all kind of token by using code */
+function getAllToken(code) {
   return new Promise((resolve, reject) => {
     co(function*() {
       var options = {
         method: 'POST',
-        uri: 'https://tutorial-h1.dev.ekoapp.com/oauth/token',
+        uri: tokenUri,
         auth: {
           'user': clientId,
           'pass': clientSecret
@@ -38,12 +41,37 @@ function getToken(code) {
   });
 }
 
+function getAccessToken(refreshToken) {
+  return new Promise((resolve, reject) => {
+    co(function*() {
+      var options = {
+        method: 'POST',
+        uri: tokenUri,
+        auth: {
+          'user': clientId,
+          'pass': clientSecret
+        },
+        form: {
+          grant_type: 'refresh_token',
+          refresh_token: refreshToken,
+          redirect_uri: redirectUri
+        },
+      };
+      var response = yield rp(options);
+      return resolve(response);
+    }).catch(function(err) {
+      return reject(err);
+    });
+  });
+}
+
+/* This function is used to get user information by using access token */
 function getUserinfo(accessToken) {
   return new Promise((resolve, reject) => {
     co(function*() {
       var options = {
         method: 'GET',
-        uri: 'https://tutorial-h1.dev.ekoapp.com/userinfo',
+        uri: userinfoUri,
         "headers": {
           "Authorization": "Bearer " + accessToken,
         }
@@ -56,29 +84,36 @@ function getUserinfo(accessToken) {
   });
 }
 
+/* This callback is used to receive authorization code from IdP */
 router.get('/cb', function(req, res, next) {
   co(function*() {
     var code = req.query.code;
-
-    var token = JSON.parse(yield getToken(code));
-    console.log(token.access_token);
-    var userinfo = yield getUserinfo(token.access_token);
-    console.log(userinfo);
-    return res.send(token.access_token);
+    var token = JSON.parse(yield getAllToken(code));
+    var id_token = jwtDecode(token.id_token);
+    var userinfo = JSON.parse(yield getUserinfo(token.access_token));
+    return res.render('index.ejs', {
+      token: token,
+      userinfo: userinfo,
+      id_token: id_token
+    });
   
   }).catch(function(err) {
     console.log(err);
+    return res.render('error.ejs', {
+      error: err
+    });
   });
 });
 
-
-router.post('/cb', function(req, res, next) {
+router.get('/access_token', function(req, res, next) {
   co(function*() {
-    console.log(req.body);    
+    var refreshToken = req.query.refresh_token;
+    console.log('refresh',refreshToken);
+    var token = JSON.parse(yield getAccessToken(refreshToken));
+    return res.send(token);
   }).catch(function(err) {
     console.log(err);
   });
-  res.send('post');
 });
 
 module.exports = router;
